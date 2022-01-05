e9c1e0d6dfac718a6c8c64a2364ae1bfd46e1798
jamc18 <72203668+jamc18@users.noreply.github.com>
2022-01-05 10:06:31 -0500

    Update ident.sh

--- ident.sh
+++ ident.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Primer comando de practicas
-#Programa en bash para saber a que maquina le hacemos pign windows o linux
+#Programa en bash para saber a que maquina le hacemos ping windows o linux
 ip=$1
 ttl=$(ping -c 1 $1|sed 's/ /\n/g'|grep "ttl"|sed 's/=/ /'|awk 'NF{print $NF}')
 if (( 64 < $ttl )) && (( 128 >= $ttl )); then
