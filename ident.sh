1a541dd6ddfaf7e6bef73fb2217ca1a8b4736a82
M4j4mc <72203668+jamc18@users.noreply.github.com>
2022-02-23 13:05:18 -0500

    Update ident.sh

--- ident.sh
+++ ident.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #Primer comando de practicas
 #Programa en bash para saber a que maquina le hacemos ping windows o linux
+echo "El argumento es ./ident.sh <ip> o <dominio>"
 ttl=$(ping -c 1 $1|sed 's/ /\n/g'|grep "ttl"|sed 's/=/ /'|awk 'NF{print $NF}')
 if (( 64 < $ttl )) && (( 128 >= $ttl )); then
 	echo "Windows->(ttl:$ttl)"
 elif (( $ttl > 0 )) && (( $ttl <=64 )); then
 	echo "Linux->(ttl:$ttl)"
-else
-	echo "El argumento es ./ident.sh <ip> o <dominio>"
 fi
