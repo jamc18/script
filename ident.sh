54b54e34640f43f35ce70c8cf99d4beaf80bfe0a
M4j4mc <72203668+jamc18@users.noreply.github.com>
2022-03-03 19:29:40 -0500

    Update ident.sh
    Mejora mostrar error si es que no tiene un solo argumento

--- ident.sh
+++ ident.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 #Primer comando de practicas
 #Programa en bash para saber a que maquina le hacemos ping windows o linux
-echo "El argumento es ./ident.sh <ip> o <dominio>"
-ttl=$(ping -c 1 $1|sed 's/ /\n/g'|grep "ttl"|sed 's/=/ /'|awk 'NF{print $NF}')
-if (( 64 < $ttl )) && (( 128 >= $ttl )); then
-	echo "Windows->(ttl:$ttl)"
-elif (( $ttl > 0 )) && (( $ttl <=64 )); then
-	echo "Linux->(ttl:$ttl)"
+#!/bin/bash
+error(){
+    echo $1
+    exit 1
+}
+if [ $# -ne 1 ]; then
+    error "[*]USO-> scrip.sh <ip objetivo o dominio>"
+fi
+ttl=$(ping -c 1 $1|sed "s/ /\n /g"|grep "ttl"|cut -d "=" -f 2)
+if (( $ttl > 64 )) && (( $ttl<=128 )); then
+    echo "Windows --> ttl:$ttl"
+elif (( $ttl <=64 )); then
+    echo "Linux --> ttl:$ttl"
 fi
