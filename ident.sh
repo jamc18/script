1f8498d7ab8e03c3ada004440755738d18284d01
jamc <jamc-18082002@hotmail.com>
2022-04-01 22:48:10 -0500

    Agregando ctrl_c

--- ident.sh
+++ ident.sh
@@ -9,6 +9,11 @@ purpleColour="\e[0;35m\033[1m"
 turquoiseColour="\e[0;36m\033[1m"
 grayColour="\e[0;37m\033[1m"
 
+trap ctrl_c INT
+function ctrl_c(){
+	echo -e "\n${redColour}[!] Saliendo.....\n${endColour}"
+	exit 1
+}
 #Funcion error
 error(){
     echo -e "${redColour}[!] Usage: $0 <ip or domain>${endColour}"
@@ -20,21 +25,12 @@ error(){
 if [ $# -ne 1 ]; then
     error
 fi
-
-#Comando verificacion
-ping -c 1 $1 >/dev/null 2>&1
-val=$(echo $?)
-
-#Condicion para verificar que la salida sea exitosa del comando ping
-#if [ $val -ne 0 ]; then
-#    error 
-#fi
 #Variable con ttl
 ttl=$(ping -c 1 $1|sed "s/ /\n /g"|grep "ttl"|cut -d "=" -f 2)
 
 #Condicion para identifiar el S.O
 if (( $ttl > 64 )) && (( $ttl<=128 )); then
-    echo "Windows --> ttl:$ttl"
+    echo -e "${greenColour}Windows --> ttl:$ttl${endColour}"
 elif (( $ttl <=64 )); then
-    echo "Linux --> ttl:$ttl"
+    echo -e "${blueColour}Linux --> ttl:$ttl${endColour}"
 fi
